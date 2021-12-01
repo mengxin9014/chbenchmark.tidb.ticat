433946d53a6600665afe4b0e96ff0670ab261195
mengxin <tregoldmeng@gmail.com>
2021-12-01 13:50:14 +0800

    add record

--- chbench/load.bash
+++ chbench/load.bash
@@ -4,11 +4,11 @@ set -euo pipefail
 env_file="${1}/env"
 env=`cat "${env_file}"`
 shift
-
-shift
-user="${1}"
-chbench_path="${2}"
-wd="${3}"
+chbench_path=`must_env_val "${env}" 'chbench.chbench_path'`
+sf=`must_env_val "${env}" 'chbench.scalefactor'`
+host=`must_env_val "${env}" 'mysql.host'`
+port=`must_env_val "${env}" 'mysql.port'`
+user=`must_env_val "${env}" 'mysql.user'`
 
 function set_tiflash()
 {
@@ -23,11 +23,6 @@ function set_tiflash()
   done
 }
 
-name=`must_env_val "${env}" 'tidb.cluster'`
-tidbs=`must_cluster_tidbs "${name}"`
-tidb=`echo "${tidbs}" | head -n 1`
-host=`echo "${tidb}" | awk -F ':' '{print $1}'`
-port=`echo "${tidb}" | awk -F ':' '{print $2}'`
 loadconfig="${chbench_path}/config/tidb/chbenchmark_config_base.xml"
 jarfile="${chbench_path}/benchbase.jar"
 url="jdbc:mysql:\/\/${host}:${port}\/benchbase?rewriteBatchedStatements=true"
@@ -35,7 +30,7 @@ tables="CUSTOMER ITEM HISTORY DISTRICT NEW_ORDER OORDER ORDER_LINE STOCK WAREHOU
 
 mysql --host ${host} --port ${port} -u ${user} -e "create database if not exists benchbase"
 cd ${chbench_path}
-cat  ${loadconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${wd}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > load_config_temp.xml
+cat  ${loadconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${sf}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > load_config_temp.xml
 java -jar ${jarfile} -b tpcc,chbenchmark -c load_config_temp.xml --create=true --load=true --execute=false &
 sleep 20
 set_tiflash benchbase "$tables" "mysql --host $host --port $port -u root -e"
--- chbench/run.bash
+++ chbench/run.bash
@@ -1,14 +1,15 @@
-set -euo pipefail
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../helper/helper.bash"
 
 env_file="${1}/env"
 env=`cat "${env_file}"`
 shift
+chbench_path=`must_env_val "${env}" 'chbench.chbench_path'`
+sf=`must_env_val "${env}" 'chbench.scalefactor'`
+host=`must_env_val "${env}" 'mysql.host'`
+port=`must_env_val "${env}" 'mysql.port'`
+user=`must_env_val "${env}" 'mysql.user'`
 
-shift
-user="${1}"
-chbench_path="${2}"
-wd="${3}"
+br_storage="${1}"
 
 function wait_table()
 {
@@ -25,11 +26,39 @@ function wait_table()
 	python2 ${chbench_path}/scripts/wait_tiflash_table_available.py "$database" $tables "$mysql_client" ; return $?
 }
 
-name=`must_env_val "${env}" 'tidb.cluster'`
-tidbs=`must_cluster_tidbs "${name}"`
-tidb=`echo "${tidbs}" | head -n 1`
-host=`echo "${tidb}" | awk -F ':' '{print $1}'`
-port=`echo "${tidb}" | awk -F ':' '{print $2}'`
+function br_wait_table()
+{
+  local database="$1"
+  local tables="$2"
+  local mysql_client="$3"
+  local tiflash_replica=1
+  local time_out=3600
+  time=0
+
+  count_table=""
+  for table in $tables
+  do
+      count_table="${count_table};select count(*) from benchbase.$table"
+  done
+
+  while true
+  do
+    if [ ${time} -eq ${time_out} ]
+        then
+          echo "br wait tiflash table failed!"
+          exit 1
+        fi
+    $mysql_client "set tidb_isolation_read_engines='tiflash'${count_table}"
+    if [ ${?} -eq 0 ]
+    then
+      break
+    else
+      sleep 1
+      time=${time}+1
+    fi
+  done
+
+}
 
 loadconfig="${chbench_path}/config/tidb/chbenchmark_config_base.xml"
 apconfig="${chbench_path}/config/tidb/querys/chbenchmark_config_ap_base.xml"
@@ -62,15 +91,19 @@ do
 
   for query in $querys
   do
-    cat  ${loadconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${wd}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > load_config_temp.xml
-    java -jar ${jarfile} -b tpcc,chbenchmark -c load_config_temp.xml --create=true --load=true --execute=false
-
-    wait_table benchbase "$tables" "mysql --host $host --port $port -u root -e"
-
-    cat ${tpconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${wd}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > tp_config_temp.xml
+    if [ ${br_storage} != "" ]
+    then
+      AWS_ACCESS_KEY_ID=minioadmin AWS_SECRET_ACCESS_KEY=minioadmin br restore db --pd 10.233.75.203:2379 --db benchbase ${br_storage} --s3.endpoint http://minio.pingcap.net:9000 --send-credentials-to-tikv=true
+      br_wait_table benchbase "$tables" "mysql --host $host --port $port -u root -e"
+    else
+      cat  ${loadconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${sf}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > load_config_temp.xml
+      java -jar ${jarfile} -b tpcc,chbenchmark -c load_config_temp.xml --create=true --load=true --execute=false
+      wait_table benchbase "$tables" "mysql --host $host --port $port -u root -e"
+    fi
+    cat ${tpconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${sf}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" > tp_config_temp.xml
     java -jar ${jarfile} -b tpcc -c tp_config_temp.xml --create=false --load=false --execute=true -d $result_dir/outputfile_tidb_query_${query}_ap_${ap}_tp &
 
-  	cat ${apconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${wd}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" | sed "s/<name>.*<\/name>/<name>${query}<\/name>/g" | sed "s/<active_terminals bench=\"chbenchmark\">.*<\/active_terminals>/<active_terminals bench=\"chbenchmark\">${ap}<\/active_terminals>/g" | sed "s/<terminals>.*<\/terminals>/<terminals>${ap}<\/terminals>/g" > ap_config_temp.xml
+  	cat ${apconfig} | sed "s/<scalefactor>.*<\/scalefactor>/<scalefactor>${sf}<\/scalefactor>/g" | sed "s/<url>.*<\/url>/<url>${url}<\/url>/g" | sed "s/<name>.*<\/name>/<name>${query}<\/name>/g" | sed "s/<active_terminals bench=\"chbenchmark\">.*<\/active_terminals>/<active_terminals bench=\"chbenchmark\">${ap}<\/active_terminals>/g" | sed "s/<terminals>.*<\/terminals>/<terminals>${ap}<\/terminals>/g" > ap_config_temp.xml
     java -jar ${jarfile} -b chbenchmark -c ap_config_temp.xml --create=false --load=false --execute=true -d $result_dir/outputfile_tidb_query_${query}_ap_${ap}_ap &
     wait
   done
